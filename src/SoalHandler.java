e2f1e4cf9d16730a38e308b2e5ade187ce55f78a
twelverakki <twelve.rakki@gmail.com>
2025-05-16 21:13:02 +0800

    Final update

--- src/SoalHandler.java
+++ src/SoalHandler.java
@@ -3,36 +3,22 @@ import java.io.*;
 
 public class SoalHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
-        String nama = exchange.getRequestURI().getQuery().split("=")[1];
-        Peserta peserta = Main.pesertaMap.get(nama);
-        Soal soal = peserta.getSoal();  // Mengambil soal terbaru setiap kali
+        String nama         = exchange.getRequestURI().getQuery().split("=")[1];
+        Peserta peserta     = Main.pesertaMap.get(nama);
+        Soal soal           = peserta.getSoal();
 
-        // Mengecek apakah sudah selesai 20 pengujian
         if (peserta.sudahSelesai()) {
-            String result = "<html><body><h1>Hasil Tes</h1>"
-                    + "<p>Nama: " + peserta.getNama() + "</p>"
-                    + "<p>Poin: " + peserta.getPoin() + "</p></body></html>";
+            String result = HTMLGenerator.headHTML + HTMLGenerator.hasilTes(peserta) + HTMLGenerator.closeHTML;
+
             exchange.sendResponseHeaders(200, result.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(result.getBytes());
             os.close();
+
             return;
         }
 
-        // Mengambil soal dengan huruf yang sudah hilang
-        String soalDenganHilang = soal.getSoalDenganHilang();
-
-        // Menyajikan soal dan pilihan jawaban.
-        String response = "<html><body>"
-                + "<h1>Soal Pengujian " + (peserta.getJumlahPengujian() + 1) + "</h1>"
-                + "<p>Soal: " + soal.getSoal() + "</p>"
-                + "<p>Isi soal: " + soalDenganHilang + "</p>"
-                + "<button type='button' onclick=\"location.href='/jawab_soal?nama=" + nama + "&jawaban=A'\">A</button>"
-                + "<button type='button' onclick=\"location.href='/jawab_soal?nama=" + nama + "&jawaban=B'\">B</button>"
-                + "<button type='button' onclick=\"location.href='/jawab_soal?nama=" + nama + "&jawaban=C'\">C</button>"
-                + "<button type='button' onclick=\"location.href='/jawab_soal?nama=" + nama + "&jawaban=D'\">D</button>"
-                + "<button type='button' onclick=\"location.href='/jawab_soal?nama=" + nama + "&jawaban=E'\">E</button>"
-                + "</body></html>";
+        String response = HTMLGenerator.headHTML + HTMLGenerator.tampilanHalamanPengujian(peserta, soal) + HTMLGenerator.closeHTML;
 
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
