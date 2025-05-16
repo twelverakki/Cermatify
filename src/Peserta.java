25971c3f812f876896be737f15f9d5327b79a659
ferastoro <ferarrimeilani@gmail.com>
2025-05-16 20:09:28 +0800

    final submit

--- src/Peserta.java
+++ src/Peserta.java
@@ -4,26 +4,35 @@ public class Peserta {
     private String nama;
     private int poin;
     private int jumlahPengujian;
-    private List<Character> grandSoal;  // Grand soal yang tetap
+    private List<Character> grandSoal;
+
     private Soal soal;
 
-    // Konstruktor yang menerima nama peserta dan grand soal untuk membuat soal pertama
     public Peserta(String nama, List<Character> grandSoal) {
         this.nama = nama;
         this.poin = 0;
         this.jumlahPengujian = 0;
-        this.grandSoal = new ArrayList<>(grandSoal);  // Grand soal tetap
-        this.soal = new Soal(grandSoal);  // Membuat soal pertama berdasarkan grand soal
+        this.grandSoal = new ArrayList<>(grandSoal);
+        this.soal = new Soal(grandSoal);
+    }
+
+    public Peserta(String nama, int poin, int jumlahPengujian) {
+        this.nama = nama;
+        this.poin = poin;
+        this.jumlahPengujian = jumlahPengujian;
+    }
+
+    public List<Character> getGrandSoal() {
+        return grandSoal;
     }
 
     public void tambahPoin() {
-        this.poin += 5;
+        this.poin += 25;
     }
 
-    // Setiap kali soal dijawab, soal baru di-generate
     public void tambahJumlahPengujian() {
         this.jumlahPengujian++;
-        this.soal = new Soal(new ArrayList<>(this.grandSoal));  // Soal baru yang dihasilkan dari grand soal yang sama
+        this.soal = new Soal(new ArrayList<>(this.grandSoal));
     }
 
     public boolean sudahSelesai() {
@@ -38,8 +47,6 @@ public class Peserta {
         return poin;
     }
 
-    //test
-
     public Soal getSoal() {
         return soal;
     }
@@ -47,5 +54,4 @@ public class Peserta {
     public int getJumlahPengujian() {
         return jumlahPengujian;
     }
-
 }
